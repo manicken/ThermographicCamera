f02c7246bb72df5fb0f671b41bea3d7f05e84eb5
manicken <65423412+manicken@users.noreply.github.com>
2022-03-25 16:15:14 +0100

    first functional thermal camera MLX+TFT

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,35 @@ Adafruit_ST7789 tft = Adafruit_ST7789(&SPI, TFT_CS, TFT_DC, TFT_RST);
 //#define PRINT_ASCIIART
 #define PRINT_TFT
 
+//the colors we will be using
+const uint16_t camColors[] = {0x480F,
+0x400F,0x400F,0x400F,0x4010,0x3810,0x3810,0x3810,0x3810,0x3010,0x3010,
+0x3010,0x2810,0x2810,0x2810,0x2810,0x2010,0x2010,0x2010,0x1810,0x1810,
+0x1811,0x1811,0x1011,0x1011,0x1011,0x0811,0x0811,0x0811,0x0011,0x0011,
+0x0011,0x0011,0x0011,0x0031,0x0031,0x0051,0x0072,0x0072,0x0092,0x00B2,
+0x00B2,0x00D2,0x00F2,0x00F2,0x0112,0x0132,0x0152,0x0152,0x0172,0x0192,
+0x0192,0x01B2,0x01D2,0x01F3,0x01F3,0x0213,0x0233,0x0253,0x0253,0x0273,
+0x0293,0x02B3,0x02D3,0x02D3,0x02F3,0x0313,0x0333,0x0333,0x0353,0x0373,
+0x0394,0x03B4,0x03D4,0x03D4,0x03F4,0x0414,0x0434,0x0454,0x0474,0x0474,
+0x0494,0x04B4,0x04D4,0x04F4,0x0514,0x0534,0x0534,0x0554,0x0554,0x0574,
+0x0574,0x0573,0x0573,0x0573,0x0572,0x0572,0x0572,0x0571,0x0591,0x0591,
+0x0590,0x0590,0x058F,0x058F,0x058F,0x058E,0x05AE,0x05AE,0x05AD,0x05AD,
+0x05AD,0x05AC,0x05AC,0x05AB,0x05CB,0x05CB,0x05CA,0x05CA,0x05CA,0x05C9,
+0x05C9,0x05C8,0x05E8,0x05E8,0x05E7,0x05E7,0x05E6,0x05E6,0x05E6,0x05E5,
+0x05E5,0x0604,0x0604,0x0604,0x0603,0x0603,0x0602,0x0602,0x0601,0x0621,
+0x0621,0x0620,0x0620,0x0620,0x0620,0x0E20,0x0E20,0x0E40,0x1640,0x1640,
+0x1E40,0x1E40,0x2640,0x2640,0x2E40,0x2E60,0x3660,0x3660,0x3E60,0x3E60,
+0x3E60,0x4660,0x4660,0x4E60,0x4E80,0x5680,0x5680,0x5E80,0x5E80,0x6680,
+0x6680,0x6E80,0x6EA0,0x76A0,0x76A0,0x7EA0,0x7EA0,0x86A0,0x86A0,0x8EA0,
+0x8EC0,0x96C0,0x96C0,0x9EC0,0x9EC0,0xA6C0,0xAEC0,0xAEC0,0xB6E0,0xB6E0,
+0xBEE0,0xBEE0,0xC6E0,0xC6E0,0xCEE0,0xCEE0,0xD6E0,0xD700,0xDF00,0xDEE0,
+0xDEC0,0xDEA0,0xDE80,0xDE80,0xE660,0xE640,0xE620,0xE600,0xE5E0,0xE5C0,
+0xE5A0,0xE580,0xE560,0xE540,0xE520,0xE500,0xE4E0,0xE4C0,0xE4A0,0xE480,
+0xE460,0xEC40,0xEC20,0xEC00,0xEBE0,0xEBC0,0xEBA0,0xEB80,0xEB60,0xEB40,
+0xEB20,0xEB00,0xEAE0,0xEAC0,0xEAA0,0xEA80,0xEA60,0xEA40,0xF220,0xF200,
+0xF1E0,0xF1C0,0xF1A0,0xF180,0xF160,0xF140,0xF100,0xF0E0,0xF0C0,0xF0A0,
+0xF080,0xF060,0xF040,0xF020,0xF800,};
+
 void setup() {
   //while (!Serial) delay(10);
   Serial.begin(115200);
@@ -44,6 +73,8 @@ void setup() {
     tft.setTextColor(ST77XX_BLUE);
     tft.setTextSize(4);
     tft.print(1234.567);
+    for (int i=0;i<10;i++)
+        tft.drawRGBBitmap(0, 200+i, camColors, 240, 1);
     
 
   Serial.println("Adafruit MLX90640 Simple Test");
@@ -94,70 +125,82 @@ void setup() {
   
 }
 
-float a, b, c, High, Low, Delta, TempC, vdd, Ta, tr, emissivity;
-int MINTEMP = 22;
-int MAXTEMP = 28;
+float minTemp = 0.0f;
+float maxTemp = 0.0f;
 
-unsigned int GetColor(float TempC) {
-    byte red=0,green=0,blue=0;
-  red = constrain(255.0 / (c - b) * TempC - ((b * 255.0) / (c - b)), 0, 255);
-  if (TempC < a) {
-    green = constrain(255.0 / (a - MINTEMP) * TempC - (255.0 * MINTEMP) / (a - MINTEMP), 0, 255);
-  }
-  else if (TempC > c) {
-    green = constrain(255.0 / (c - MAXTEMP) * TempC - (MAXTEMP * 255.0) / (c - MAXTEMP), 0, 255);
-  }
-  else {
-    green = 255;
-  }
-  if (TempC > (MAXTEMP )) {
-    blue = constrain((TempC - MAXTEMP) * 55.0, 0, 255);
-  }
-  else {
-    blue = constrain(255.0 / (a - b) * TempC - ((a * 255.0) / (a - b)), 0, 255);
-  }
-
-  return tft.color565(red, green, blue);
+void getMinMaxTemps()
+{
+    minTemp = 500.0f;
+    maxTemp = -40.0f;
+    for (uint8_t h=0; h<24; h++) {
+        for (uint8_t w=0; w<32; w++) {
+            float t = frame[h*32 + w];
+            if (t > maxTemp)
+                maxTemp = t;
+            if (t < minTemp)
+                minTemp = t;
+        }
+    }
 }
 
+
+
 void loop() {
-  //delay(100);
-  unsigned long myTime = millis();
-  int read_status = mlx.getFrame(frame);
-  if (read_status != 0) {
-    Serial.printf("Failed:%d", read_status);
-    return;
-  }
-  Serial.printf("mlx.getFrame time:%d\n",(millis()-myTime));
-  Serial.println();
-  Serial.println();
-  for (uint8_t h=0; h<24; h++) {
-    for (uint8_t w=0; w<32; w++) {
-      float t = frame[h*32 + w];
-#ifdef PRINT_TFT
+    //delay(100);
+    unsigned long myTime = millis();
+    int read_status = mlx.getFrame(frame);
+    if (read_status != 0) {
+        Serial.printf("Failed:%d", read_status);
+        return;
+    }
+    //Serial.printf("mlx.getFrame time:%d\n",(millis()-myTime));
+    //Serial.println();
+    //Serial.println();
+    getMinMaxTemps();
     
+    // clear prev text
+    tft.fillRect(0, 180, 84, 14, ST77XX_BLACK);
+    tft.fillRect(180, 180, 60, 14, ST77XX_BLACK);
+
+    // print new text
+    tft.setTextSize(2);
+    tft.setTextColor(ST77XX_WHITE);
+    tft.setCursor(0, 180);
+    tft.print(minTemp);
+    tft.setCursor(180, 180);
+    tft.print(maxTemp);
+
+    
+    for (uint8_t h=0; h<24; h++) {
+        for (uint8_t w=0; w<32; w++) {
+            float t = frame[h*32 + w];
+#ifdef PRINT_TFT
+            uint8_t colorIndex = map(t, minTemp, maxTemp, 0, 255);
+            colorIndex = constrain(colorIndex, 0, 255);
+
+            tft.fillRect(w*7, h*7, 7, 7, camColors[colorIndex]);
 #endif
 // keep the following for debugging purposes
 #ifdef PRINT_TEMPERATURES
-      Serial.print(t, 1);
-      Serial.print(", ");
+            Serial.print(t, 1);
+            Serial.print(", ");
 #endif
 #ifdef PRINT_ASCIIART
-      //char c = '&';
-      if (t < 20) Serial.print("  ");// c = ' ';
-      else if (t < 23) Serial.print("..");// c = '.';
-      else if (t < 25) Serial.print("--");// c = '-';
-      else if (t < 27) Serial.print("**");// c = '*';
-      else if (t < 29) Serial.print("++");// c = '+';
-      else if (t < 31) Serial.print("xx");// c = 'x';
-      else if (t < 33) Serial.print("%%");// c = '%';
-      else if (t < 35) Serial.print("##");// c = '#';
-      else if (t < 37) Serial.print("XX");// c = 'X';
-      else Serial.print("&&");
+            //char c = '&';
+            if (t < 20) Serial.print("  ");// c = ' ';
+            else if (t < 23) Serial.print("..");// c = '.';
+            else if (t < 25) Serial.print("--");// c = '-';
+            else if (t < 27) Serial.print("**");// c = '*';
+            else if (t < 29) Serial.print("++");// c = '+';
+            else if (t < 31) Serial.print("xx");// c = 'x';
+            else if (t < 33) Serial.print("%%");// c = '%';
+            else if (t < 35) Serial.print("##");// c = '#';
+            else if (t < 37) Serial.print("XX");// c = 'X';
+            else Serial.print("&&");
+#endif
+        }
+#if defined(PRINT_ASCIIART) || defined(PRINT_TEMPERATURES)
+        Serial.println();
 #endif
     }
-#if defined(PRINT_ASCIIART) || defined(PRINT_TEMPERATURES)
-    Serial.println();
-#endif
-  }
 }
